fabea2b93be99e041b6e6b5076802e94714abaf0
Alim Akbiev <akbievalim0578@gmail.com>
2025-01-25 03:15:01 +0300

    fix: change filters

--- books/filters.py
+++ books/filters.py
@@ -4,18 +4,17 @@ from .models import Books, Genres, Authors
 
 
 class BookFilter(django_filters.FilterSet):
+    genre = django_filters.ModelChoiceFilter(
+        field_name='genre',
+        queryset=Genres.objects.all(),
+        label='Жанр'
+    )
+    author = django_filters.ModelChoiceFilter(
+        field_name='author',
+        queryset=Authors.objects.all(),
+        label='Автор'
+    )
 
     class Meta:
         model = Books
         fields = '__all__'
-
-    genre = django_filters.TypedChoiceFilter(
-        field_name='genre__name',
-        choices=[(genre.name, genre.id) for genre in Genres.objects.all()],
-        label='Жанр'
-    )
-    author = django_filters.TypedChoiceFilter(
-        field_name='author__name',
-        choices=[(author.name, author.id) for author in Authors.objects.all()],
-        label='Автор'
-    )
